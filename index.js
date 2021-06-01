5d4bf9d740fa750f6b3231a458f886f39fa340be
julianxlozano <julianxlozano@gmail.com>
2021-06-01 12:46:05 -0500

    commit tree 65089607b146fd45cbbfa30cb7946b7419d8faf0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function wrapAdjective(string="*"){
+    return function(param="special"){
+        return `You are ${string}${param}${string}!`
+    }
+}
+
+const Calculator = {
+    'add':function(num1,num2){return num1 + num2},
+    'subtract':function(num1,num2){return num1 - num2},
+    'multiply':function(num1,num2){return num1 * num2},
+    'divide':function(num1,num2){return num1 / num2}
+};
+
+function actionApplyer(int,fnArray){
+    if (fnArray.length === 0){
+        return int
+    }else{
+
+
+     //  return fnArray[2](fnArray[1](fnArray[0](int)))
+
+        return fnArray.reduce(function(prevFnValue,currentFn){return currentFn(prevFnValue)},13)
+    };
+};
